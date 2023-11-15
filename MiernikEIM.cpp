2653304d66d349691374e2f558a38a1495526d09
Cegla <kamilceglarski2002@gmail.com>
2023-11-15 19:51:17 +0100

    SerialPort, FileInitializer, Variables classes

--- MiernikEIM.cpp
+++ MiernikEIM.cpp
@@ -16,11 +16,12 @@ int main()
 
 	for (size_t i = 0; i < currentValuesTab1Size; i++)
 	{
-		outFile << "hello";
+		outFile << "hello"; // Fix this problem LNK2005, LNK1169
 	}
 
 #else 
-	if (serial.openSerialPort()) {
+	if (serial.openSerialPort()) 
+	{
 		const char* order = "your_order";
 
 		double measurement = serial.getMeasValue(10, 1.0, order, 0.001); // Example of a function call
@@ -30,64 +31,64 @@ int main()
 
 		// Do something with the measurement
 		
-	} else {
+	} 
+	else 
+	{
 		std::cout << "Failed to open serial port.\n";
 	}
 
 
-	char  order[30];
+	//char  order[30];
 
-	for (int j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
-	{
-		sprintf_s(order, "VOLT2 %f\n", voltsValuesTab1[j]);
-		serial.writeOrder(order);
+	//for (int j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
+	//{
+	//	sprintf_s(order, "VOLT2 %f\n", voltsValuesTab1[j]);
+	//	serial.writeOrder(order);
 
-		Sleep(2500); // [ms]
+	//	Sleep(2500); // [ms]
 
-		for (int i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
-		{
-			sprintf_s(order, "CURR %f\n", currentValuesTab1[i]);
-			serial.writeOrder(order);
+	//	for (int i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
+	//	{
+	//		sprintf_s(order, "CURR %f\n", currentValuesTab1[i]);
+	//		serial.writeOrder(order);
 
-			Sleep(1000);
+	//		Sleep(1000);
 
-			double meas = serial.getMeasValue(40, 0.1, "MEAS:CURR? (@2)\n", 0.0001);
-			measValues[j][i] = -meas;
+	//		double meas = serial.getMeasValue(40, 0.1, "MEAS:CURR? (@2)\n", 0.0001);
+	//		measValues[j][i] = -meas;
 
-			double measVolts = serial.getMeasValue(40, 0.1, "MEAS:VOLT? (@3)\n", 0.0001);
-			measValuesVolts[j][i] = measVolts;
-		}
-	}
+	//		double measVolts = serial.getMeasValue(40, 0.1, "MEAS:VOLT? (@3)\n", 0.0001);
+	//		measValuesVolts[j][i] = measVolts;
+	//	}
+	//}
 
-	outFile << "Uce [V] ; Ube ; Ic ; Ube ; Ic ; Ube ; Ic \n";
-	
+	//outFile << "Uce [V] ; Ube ; Ic ; Ube ; Ic ; Ube ; Ic \n";
+	//
 
-	for (size_t j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
-	{
-		outFile << voltsValuesTab1[j];
+	//for (size_t j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
+	//{
+	//	outFile << voltsValuesTab1[j];
 
-		for (size_t i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
-		{
-			outFile << ';' << measValuesVolts[j][i];
-		}
-		outFile << "\n";
-	}
+	//	for (size_t i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
+	//	{
+	//		outFile << ';' << measValuesVolts[j][i];
+	//	}
+	//	outFile << "\n";
+	//}
 
-	outFile << "\n\n\n";
-	
-	for (size_t j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
-	{
-		outFile << voltsValuesTab1[j];
+	//outFile << "\n\n\n";
+	//
+	//for (size_t j = 0; j < sizeof(voltsValuesTab1) / sizeof(double); j++)
+	//{
+	//	outFile << voltsValuesTab1[j];
 
-		for (size_t i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
-		{
-			outFile << ';' << measValues[j][i];
-		}
-		outFile << "\n";
-	}
+	//	for (size_t i = 0; i < sizeof(currentValuesTab1) / sizeof(double); i++)
+	//	{
+	//		outFile << ';' << measValues[j][i];
+	//	}
+	//	outFile << "\n";
+	//}
 #endif
 
-
 	return 0;
-
 }
