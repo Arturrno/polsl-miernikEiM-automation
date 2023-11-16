6cd3fbcfc16255b9b39c2c25bb90d99aff6aceca
Cegla <kamilceglarski2002@gmail.com>
2023-11-16 21:07:41 +0100

    test

--- MiernikEIM.cpp
+++ MiernikEIM.cpp
@@ -14,7 +14,7 @@ int main()
 
 	for (size_t i = 0; i < currentValuesTab1Size; i++)
 	{
-		outFile << "HEllo";
+		outFile << "Hello World";
 		serial.setCurr(1.2);
 		double measurement = serial.getVolt(); 
 	}
